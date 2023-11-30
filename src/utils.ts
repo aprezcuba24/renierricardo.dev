6c06a2aceb8a2613beba0fa268ba0450e0676925
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2023-11-29 20:26:46 -0500

    Last posts

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,11 @@ dayjs.locale("es");
 
 export const formatDate = (date: string) => dayjs(date).format("D MMM. YYYY");
 export const sortByDate = (a: any, b: any) => dayjs(b).valueOf() - dayjs(a).valueOf();
+export const transformPosts = (posts: any) => posts.sort((a: any, b: any) => sortByDate(a.frontmatter.pubDate, b.frontmatter.pubDate))
+.map(({ frontmatter: { title, pubDate, description, image }, url }: any) => ({
+  title,
+  url,
+  description,
+  image,
+  pubDate: formatDate(pubDate),
+}));
